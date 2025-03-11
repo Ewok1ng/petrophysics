3cd1e6cd7df26d41ecbc2704bfca302a93d53afd
Ewok1ng <joker187566@gmail.com>
2025-03-11 17:33:55 +0300

    feat: init

---
+++ src/app/app.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import 'virtual:svg-icons-register';
+
+import './styles/styles.scss';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<div>App</div>
+	</React.StrictMode>
+);
